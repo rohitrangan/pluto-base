b59fa5734ac089d038c7563daf7a3077a3c233c2
Rohit Rangan <rohitrangan@gmail.com>
2012-07-12 11:16:07 +0530

    All renamed files now compile properly. DCM also updated according to the new SensorData structures.

--- Pluto-chibios-2.5/src/DCM/dcm/dcm_update.c
+++ Pluto-chibios-2.5/src/DCM/dcm/dcm_update.c
@@ -12,18 +12,11 @@
 // TODO - Print the gyro, accelerometer and magnetometer values separately
 
 #if PLUTO_USE_DCM && PLUTO_USE_MAGNETOMETER && PLUTO_USE_IMU
-float mag_final[3], imu_data[6];
-
-float calc_gyro_rate(int16_t raw, float sens){
-  float tmp = (float)raw;
-  tmp /= sens;
-  return tmp;
-}
 
 static WORKING_AREA(waUpdate, 512);
 static msg_t Update(void *arg) {
 
-	chRegSetThreadName("DCMUpdate");
+	chRegSetThreadName("DCMUpdate") ;
 	float dcmMatrix[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0 ,1}} ;
 	uint32_t start, end ;
 	uint8_t first = 1 ;
@@ -33,16 +26,16 @@ static msg_t Update(void *arg) {
 			start = halGetCounterValue() ;
 			first = 0 ;
 		}
-		readAllIMUData(imu_data) ; /* Reads values from the accelerometer and gyroscope */
+		readAllIMUData(&IMUD1) ; /* Reads values from the accelerometer and gyroscope */
 		chThdSleepMilliseconds(10) ;
 
-		magGetScaledData(mag_final) ; /* Reads values from the magnetometer */
+		readMagnetometerData(&MD1) ; /* Reads values from the magnetometer */
 		chThdSleepMilliseconds(10) ;
 		end = halGetCounterValue() ;
 		interval = convertCounterToMilliseconds(start, end) / 1000.0f ;
 		start = end ;
 
-		dcmUpdate(dcmMatrix, imu_data[0], imu_data[1], imu_data[2], imu_data[3], imu_data[4], imu_data[5], mag_final[0], mag_final[1], mag_final[2], interval);
+		dcmUpdate(dcmMatrix, IMUD1.ACCEL_X, IMUD1.ACCEL_Y, IMUD1.ACCEL_Z, IMUD1.GYRO_X, IMUD1.GYRO_Y, IMUD1.GYRO_Z, MD1.MAG_X, MD1.MAG_Y, MD1.MAG_Z, interval) ;
 
 		if(dcmMatrix[2][2] >= 0) {
 			pitch = -asinf(dcmMatrix[2][0]) ;
@@ -59,14 +52,14 @@ static msg_t Update(void *arg) {
 		yaw   = (180 * yaw  ) / M_PI ;
 
 		chprintf((BaseSequentialStream *)arg, "Interval:- %f ms ", (interval * 1000.0f)) ;
-		chprintf((BaseSequentialStream *)arg, "Pitch : %f\t\t", pitch);
-		chprintf((BaseSequentialStream *)arg, "Roll  : %f\t\t", roll );
-		chprintf((BaseSequentialStream *)arg, "Yaw   : %f\r\n", yaw  );
+		chprintf((BaseSequentialStream *)arg, "Pitch : %f\t\t", pitch) ;
+		chprintf((BaseSequentialStream *)arg, "Roll  : %f\t\t", roll ) ;
+		chprintf((BaseSequentialStream *)arg, "Yaw   : %f\r\n", yaw  ) ;
 	}
-	return(0);
+	return 0 ;
 }
 
-void startDCMThread(BaseSequentialStream *bss){
-	chThdCreateStatic(waUpdate, sizeof(waUpdate), HIGHPRIO, Update, bss);
+void startDCMThread(BaseSequentialStream *bss) {
+	chThdCreateStatic(waUpdate, sizeof(waUpdate), HIGHPRIO, Update, bss) ;
 }
 #endif	/*PLUTO_USE_DCM && PLUTO_USE_MAGNETOMETER && PLUTO_USE_IMU */
